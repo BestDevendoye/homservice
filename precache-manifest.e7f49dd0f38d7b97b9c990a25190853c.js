19df51b8732ba229e67aa459d22c5ba5670a8fc6
deve ndoye <daouda.ndoye25@gmail.com>
2020-10-11 17:25:04 +0200

    Updates

---
+++ precache-manifest.e7f49dd0f38d7b97b9c990a25190853c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01eaa298ee3e9d372fb139120483f47e",
+    "url": "/homservice/index.html"
+  },
+  {
+    "revision": "100c5ea41c150c3abbab",
+    "url": "/homservice/static/css/main.ac94e425.chunk.css"
+  },
+  {
+    "revision": "79b35e789edf6d653e6a",
+    "url": "/homservice/static/js/2.355adb3e.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/homservice/static/js/2.355adb3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100c5ea41c150c3abbab",
+    "url": "/homservice/static/js/main.5991491a.chunk.js"
+  },
+  {
+    "revision": "9a6ded587d4d1f9e18df",
+    "url": "/homservice/static/js/runtime-main.f07fe675.js"
+  },
+  {
+    "revision": "a23e804265f4b1cba5cf60cee515ddf3",
+    "url": "/homservice/static/media/deadpool.a23e8042.png"
+  },
+  {
+    "revision": "248392c9463115f69738082636d79a7e",
+    "url": "/homservice/static/media/ironman.248392c9.png"
+  },
+  {
+    "revision": "6a1254d8df2375f4ae56aee5ae8bab09",
+    "url": "/homservice/static/media/spiderman.6a1254d8.png"
+  }
+]);
